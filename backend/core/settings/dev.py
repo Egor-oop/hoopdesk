4551464a2a1ebabd556de06fee18348fe8cae1a2
Egor-oop <gulidoegor@gmail.com>
2023-12-01 21:33:13 +0300

    added docker-compose.dev.yml; added Dockerfile.prod; settings devinded into base, dev, prod; configured nginx in prod mode

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -1,3 +1,12 @@
 from .base import *
 
 print('DEVELOPMENT!')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = path.join(BASE_DIR, "../", "mediafiles")
+
+STATIC_URL = "static/"
+STATIC_ROOT = path.join(BASE_DIR, "../", "staticfiles")
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,4 +11,7 @@ echo 'PostgreSQL started'
 echo 'Running migrations...'
 python manage.py migrate
 
+echo 'Collecting static files...'
+python manage.py collectstatic --no-input
+
 exec "$@"
