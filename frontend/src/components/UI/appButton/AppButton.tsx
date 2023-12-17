45016107d8a4eac0c78a81bb3694f2a4a6e14759
Egor-oop <gulidoegor@gmail.com>
2023-12-17 14:56:21 +0300

    added onSubmit and onClick in appButton

--- frontend/src/components/UI/appButton/AppButton.tsx
+++ frontend/src/components/UI/appButton/AppButton.tsx
@@ -3,12 +3,16 @@ import { FC, ReactNode } from 'react'
 type TAppButtonProps = {
   children: ReactNode
   type: 'submit' | 'reset' | 'button'
+  onSubmit?: () => {}
+  onClick?: () => {}
 }
 
-export const AppButton: FC<TAppButtonProps> = ({ children, type }) => {
+export const AppButton: FC<TAppButtonProps> = ({ children, type, onSubmit, onClick }) => {
   return (
     <button
       className='bg-indigo-700 text-slate-50 px-5 py-3 rounded-lg text-sm font-medium hover:bg-indigo-600 active:bg-indigo-800 hover:cursor-pointer'
+      onSubmit={onSubmit}
+      onClick={onClick}
       type={type}>
       {children}
     </button>
