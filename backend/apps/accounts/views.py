70d256dc0a16ca48d6bdbb776c4e223b3ab8f0e2
Egor-oop <gulidoegor@gmail.com>
2023-12-17 16:19:47 +0300

    added api/me endpoint

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,12 +1,30 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, mixins, viewsets
 
 from django.contrib.auth.models import User
 
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserSerializer
+from .permissions import IsCurrentUserOrReadOnly
+
+
+class AccountViewSet(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsCurrentUserOrReadOnly,)
+
+
+class MeAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserRegisterAPIView(CreateAPIView):
