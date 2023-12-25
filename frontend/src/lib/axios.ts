0dff4d602315be4ff667320ab804e2636c43b845
Egor-oop <gulidoegor@gmail.com>
2023-12-25 22:47:47 +0300

    added interceptor for refreshing tokens

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -14,3 +14,26 @@ axiosApi.interceptors.request.use(
     Promise.reject(error)
   }
 )
+
+axiosApi.interceptors.response.use(
+  response => response,
+  error => {
+    const originalRequest = error.config
+    const refreshToken = storage.getToken().refresh
+
+    if (error.response.status === 401 && refreshToken) {
+      axiosApi.post('/api/token/refresh/')
+        .then(response => {
+          storage.setToken(response.data.access, response.data.refresh)
+          originalRequest.headers.Authorization = `Bearer ${response.data.access}`
+          axiosApi(originalRequest)
+            .then(response => {
+              return response.data
+            })
+            .catch(error => {
+              return error
+            })
+        })
+    }
+  }
+)
