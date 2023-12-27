fd554e02fd9580f6f30eddbf49d09ba46360c307
Egor-oop <gulidoegor@gmail.com>
2023-12-27 18:55:13 +0300

    removed unknown comments

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -31,28 +31,3 @@ axiosApi.interceptors.response.use(
     return Promise.reject(error)
   }
 )
-
-// axiosApi.interceptors.response.use(
-//   response => response,
-//   error => {
-//     const originalRequest = error.config
-//     const refreshToken = storage.getToken().refresh
-
-//     if (error.response.status === 401 && refreshToken) {
-//       debugger
-//       axiosApi.post('/api/token/refresh/', { refresh: refreshToken })
-//         .then(response => {
-//           storage.setToken(response.data.access, response.data.refresh)
-//           originalRequest.headers.Authorization = `Bearer ${response.data.access}`
-//           console.log(originalRequest)
-//           return axiosApi(originalRequest)
-//             .then(response => response)
-//             .catch(error => error)
-//         })
-//         .catch(error => {
-//           return error
-//         })
-//     }
-//     return Promise.reject(error);
-//   }
-// )
