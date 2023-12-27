cd2d9883c014429d275c8d00a78be3317cb615e9
Egor-oop <gulidoegor@gmail.com>
2023-12-27 18:39:43 +0300

    refresh token lifetime raised up to 90 days

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -16,27 +16,43 @@ axiosApi.interceptors.request.use(
 )
 
 axiosApi.interceptors.response.use(
-  response => response,
-  error => {
+  response => { return response },
+  async (error) => {
     const originalRequest = error.config
-    const refreshToken = storage.getToken().refresh
-
-    if (error.response.status === 401 && refreshToken) {
-      axiosApi.post('/api/token/refresh/')
-        .then(response => {
-          storage.setToken(response.data.access, response.data.refresh)
-          originalRequest.headers.Authorization = `Bearer ${response.data.access}`
-          axiosApi(originalRequest)
-            .then(response => {
-              return response.data
-            })
-            .catch(error => {
-              return error
-            })
-        })
-        .catch(error => {
-          return error
-        })
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      const refreshToken = storage.getToken().refresh
+      const newTokens = await axiosApi.post('/api/token/refresh/', {refresh: refreshToken})
+      storage.setToken(newTokens.data.access, newTokens.data.refresh)
+      axiosApi.defaults.headers.common['Authorization'] = `Bearer ${newTokens.data.refresh}`
+      console.log(newTokens)
+      return axiosApi(originalRequest)
     }
+    return Promise.reject(error)
   }
 )
+
+// axiosApi.interceptors.response.use(
+//   response => response,
+//   error => {
+//     const originalRequest = error.config
+//     const refreshToken = storage.getToken().refresh
+
+//     if (error.response.status === 401 && refreshToken) {
+//       debugger
+//       axiosApi.post('/api/token/refresh/', { refresh: refreshToken })
+//         .then(response => {
+//           storage.setToken(response.data.access, response.data.refresh)
+//           originalRequest.headers.Authorization = `Bearer ${response.data.access}`
+//           console.log(originalRequest)
+//           return axiosApi(originalRequest)
+//             .then(response => response)
+//             .catch(error => error)
+//         })
+//         .catch(error => {
+//           return error
+//         })
+//     }
+//     return Promise.reject(error);
+//   }
+// )
