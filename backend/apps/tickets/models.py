af973511e2b4b53931b079507d648d62bf65968e
Egor-oop <gulidoegor@gmail.com>
2024-01-07 11:37:00 +0300

    fixed ticket model

--- backend/apps/tickets/models.py
+++ backend/apps/tickets/models.py
@@ -6,5 +6,5 @@ from apps.clients.models import Client
 
 class Ticket(models.Model):
     title = models.CharField(max_length=998, null=False, blank=False)
-    reliable = models.ForeignKey(User, on_delete=models.SET_NULL)
+    reliable = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
