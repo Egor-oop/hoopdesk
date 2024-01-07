7a55f8e4666a2418b856cdfa81cd39ac68eed7eb
Egor-oop <gulidoegor@gmail.com>
2024-01-07 12:01:50 +0300

    ticket model fixed for the second time

--- backend/apps/tickets/models.py
+++ backend/apps/tickets/models.py
@@ -8,3 +8,5 @@ class Ticket(models.Model):
     title = models.CharField(max_length=998, null=False, blank=False)
     reliable = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
