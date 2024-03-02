2993c4b6e8fb352ab8fc38a148174461de44e850
Egor-oop <gulidoegor@gmail.com>
2024-03-02 14:46:31 +0300

    added OrderingFilter

--- backend/apps/clients/views.py
+++ backend/apps/clients/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 
 from .serializers import OrganizationSerializer, ClientSerializer, ClientSerializerOrganizationName
 from .models import Organization, Client
@@ -7,15 +8,16 @@ from .models import Organization, Client
 class OrganizationViewSet(ModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+    filter_backends = (OrderingFilter,)
 
 
 class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    filter_backends = (OrderingFilter,)
 
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
         if self.request.query_params.get('orgname') == 'true':
             serializer = ClientSerializerOrganizationName
         return serializer
-
