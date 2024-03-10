f7d099a052c8c65f3ecade80c2aaf24e51da9df1
Egor-oop <gulidoegor@gmail.com>
2024-03-10 20:07:52 +0300

    added default pagination and filtering

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -133,7 +133,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'PAGE_SIZE': 15
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 15,
+    'DEFAULT_FILTER_BACKENDS': ['rest_framework.filters.OrderingFilter'],
 }
 
 SIMPLE_JWT = {
