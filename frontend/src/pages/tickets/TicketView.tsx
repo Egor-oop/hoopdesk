309a9b3656e0ea93a79b349a8a757ec23203475b
Egor-oop <gulidoegor@gmail.com>
2024-05-09 08:20:36 +0300

    websockets

--- frontend/src/pages/tickets/TicketView.tsx
+++ frontend/src/pages/tickets/TicketView.tsx
@@ -1,15 +1,24 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { TicketDialog, TicketDetails } from '../../components'
 
 export const TicketView: FC = () => {
   const { id } = useParams()
   const navigate = useNavigate()
 
+  useEffect(() => {
+    if (id === undefined) {
+      navigate(-1)
+    }
+  }, [])
+
   return (
     <div className='flex lg:flex-row flex-col-reverse h-full max-w-full '>
-      <TicketDialog />
-      <TicketDetails />      
+      {id !== undefined ? <>
+        <TicketDialog ticketId={Number(id)} />
+        <TicketDetails />
+      </> : ''
+      }
     </div>
   )
 }
