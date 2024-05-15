ebe05ca0d2161964d43f0b3b538b94f8f8428949
Egor-oop <gulidoegor@gmail.com>
2024-05-15 13:29:58 +0300

    redux logout

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,12 +1,16 @@
+import { useDispatch } from 'react-redux'
 import { AppButton } from '../components'
 import { storage } from '../utils'
 import { useNavigate } from 'react-router-dom'
+import { logoutUser } from '../state/user/userSlice'
 
 export const Profile = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const logout = () => {
     storage.clearToken()
+    dispatch(logoutUser())
     navigate(0)
   }
 
